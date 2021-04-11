44f2744ae7af8e15ad13fb58c6c28de6e3591e2b
Andy Qiao <77704448+aqiao-814@users.noreply.github.com>
2021-04-11 10:58:01 -0500

    added the javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-function init(){
+function init() {
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  var entry = document.getElementById("entrybutton");
+  function myFunction() {
+    var textBox = document.getElementById("entryinput");
+    var output = document.getElementById("textoutput");
+    alert("Andy Qiao: " + textBox.value);
+    output.innerHTML = textBox.value;
+  }
+  entry.addEventListener('click', myFunction);
+}
 window.addEventListener('load', init);
